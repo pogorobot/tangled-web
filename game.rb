daf4b7f98751453764135a32f4cad3761d6e7a61
Josh Eklund <joshe@hranswerlink.com>
2014-12-20 23:58:03 -0800

    Choice structure is more modular and random. Different rooms have different options! You can be trapped! yaayay

--- game.rb
+++ game.rb
@@ -3,36 +3,47 @@ require_relative 'room'
 
 class Game
   attr_accessor :room
+  attr_accessor :exiting
 
   def game_loop
     orient
     describe_room
-    choice = player_input "Say Hello", "Say Goodbye", "Move On"
-    if choice == '3'
-      leave_room
-    end
-    game_loop unless choice == '2'
+    offer_choices
+    game_loop unless exiting
   end
 
   def orient
     self.room ||= Room.new('words/room_descriptions.txt')
   end
 
-  def leave_room
-    self.room = nil
-  end
-
   def describe_room
     puts self.room.description
   end
 
-  def player_input(*choices)
+  def offer_choices
+    choice = player_input self.room.choices
+    result = self.room.result(choice)
+    self.send(result) if result
+  end
+
+  def leave_room
+    self.room = nil
+  end
+
+  def player_input(choices)
     i = 1
     choices.each do |choice|
       puts "#{i}. #{choice}"
       i = i + 1
     end
     gets.chomp
+  end
+
+  def do_nothing
+  end
+
+  def exit_loop
+    self.exiting = true
   end
 end
 
--- room.rb
+++ room.rb
@@ -1,13 +1,40 @@
 class Room
-  attr_accessor :description
+  attr_accessor :description, :choices
 
   def initialize(filename)
-    possible_descriptions = []
-    File.open(filename).each do |line|
-      line = line.chomp
-      possible_descriptions << line if line.length > 0
-    end
+    self.description = random_description_from(filename)
+    self.choices = random_subset_from(choices_filename)
+  end
 
-    self.description = possible_descriptions[rand(possible_descriptions.length).floor]
+  def choices_filename
+    'words/choices.txt'
+  end
+
+  def random_description_from(filename)
+    possible_descriptions = File.open(filename).select do |line|
+      line.chomp.length > 0
+    end
+    possible_descriptions[rand(possible_descriptions.length).floor]
+  end
+
+  def random_subset_from(filename)
+    File.open(filename).select do |line|
+      line.chomp.length > 0 && rand(100) < 75
+    end
+  end
+
+  def choice(input)
+    choices[input.to_i - 1].chomp
+  end
+
+  def results
+    {
+      "Say Goodbye" => :exit_loop,
+      "Move On" => :leave_room
+    }
+  end
+
+  def result(input)
+    results[choice(input)]
   end
 end
