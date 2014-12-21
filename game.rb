a716c6377168ef96d3ac41e81f7b3f913fb866bc
Josh Eklund <joshe@hranswerlink.com>
2014-12-20 23:03:05 -0800

    Some more different kinds of things can happen. Whatever.

--- game.rb
+++ game.rb
@@ -1,23 +1,39 @@
 require 'pry'
 require_relative 'room'
 
-def game_loop
-  describe_room(Room.new('words/room_descriptions.txt'))
-  choice = player_input "Say Hello", "Say Goodbye"
-  game_loop unless choice == '2'
-end
+class Game
+  attr_accessor :room
 
-def describe_room(room)
-  puts room.description
-end
-
-def player_input(*choices)
-  i = 1
-  choices.each do |choice|
-    puts "#{i}. #{choice}"
-    i = i + 1
+  def game_loop
+    orient
+    describe_room
+    choice = player_input "Say Hello", "Say Goodbye", "Move On"
+    if choice == '3'
+      leave_room
+    end
+    game_loop unless choice == '2'
   end
-  gets.chomp
+
+  def orient
+    self.room ||= Room.new('words/room_descriptions.txt')
+  end
+
+  def leave_room
+    self.room = nil
+  end
+
+  def describe_room
+    puts self.room.description
+  end
+
+  def player_input(*choices)
+    i = 1
+    choices.each do |choice|
+      puts "#{i}. #{choice}"
+      i = i + 1
+    end
+    gets.chomp
+  end
 end
 
-game_loop
+Game.new.game_loop
